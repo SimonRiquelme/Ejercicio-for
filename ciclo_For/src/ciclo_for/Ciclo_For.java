606d5cae912d29c75c5a6aa69ca37205b76a2d54
SimonRiquelme <simon.riquelmer@gmail.com>
2015-11-09 02:22:13 -0300

    Agregando Proyecto Java

---
+++ ciclo_For/src/ciclo_for/Ciclo_For.java
@@ -0,0 +1,17 @@
+
+package ciclo_for;
+
+
+public class Ciclo_For {
+
+    public static void main(String[] args) {
+    
+    int fin=10;
+    int tabla=9;
+        
+    for (int i = 0; i <= fin; i++) {
+            System.out.println(tabla*i);
+        }
+    }
+    
+}
