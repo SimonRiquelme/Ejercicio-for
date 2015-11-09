43d041c5993e7e9bb43f40fe4616dae07909c636
SimonRiquelme <simon.riquelmer@gmail.com>
2015-11-09 03:23:50 -0300

    Ejemploi
    akjskjsad

--- ciclo_For/src/ciclo_for/Ciclo_For.java
+++ ciclo_For/src/ciclo_for/Ciclo_For.java
@@ -1,24 +1,28 @@
 
-package ciclo_for;
+  package ciclo_for;
 
-import javax.swing.text.StyledEditorKit;
+  import javax.swing.text.StyledEditorKit;
 
 
-public class Ciclo_For {
+  public class Ciclo_For {
 
-    public static void main(String[] args) {
-    
-    int fin=10;
-    int tabla=9;
-        
-    for (int i = 0; i <= fin; i++) {
-            System.out.println(tabla*i);
-        }
-    
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hola");
-        }
-     system.out.println("Hello Moto");   
-    }
-    
-}
+      public static void main(String[] args) {
+
+      int fin=10;
+      int tabla=9;
+      String palabra="Hola";
+      for (int i = 0; i <= fin; i++) {
+              System.out.println(tabla*i);
+          }
+
+          for (int i = 0; i < 10; i++) {
+              System.out.println("Hola");
+          }
+       system.out.println("Hello Moto");
+
+      }
+      if (palabra.equals("Holas")) {
+        system.out.println("Hola Mundo");
+      }
+
+  }
